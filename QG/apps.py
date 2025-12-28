ff9d5300650383bbef924253b85981e30a6daa61
MohamedGad <138883562+II-MohamedGad-II@users.noreply.github.com>
2025-12-28 15:38:01 +0200

    Delete QG directory

--- QG/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QgConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'QG'
--- QG/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import MCQQuestion
-
-class MCQQuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MCQQuestion
-        fields = '__all__'
-
-
